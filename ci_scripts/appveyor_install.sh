5ca3e0bc1977ebf3e34328c881f38b7a791123e6
Nic30 <nic30@seznam.cz>
2019-01-01 21:27:52 +0100

    appveyor install by setup-x86_64.exe

--- ci_scripts/appveyor_install.sh
+++ ci_scripts/appveyor_install.sh
@@ -1,9 +1,3 @@
-git clone https://github.com/transcode-open/apt-cyg
-cd apt-cyg
-install apt-cyg /bin
-cd ..
-
-apt-cyg install autotools
 
 git clone http://git.veripool.org/git/verilator
 cd verilator
