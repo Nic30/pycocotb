6f4a1744494a424591406c1ff6f5df7bb66a1940
Nic30 <nic30@seznam.cz>
2019-01-01 22:00:27 +0100

    appveyor install dependencies in appveyor_install.sh

--- ci_scripts/appveyor_install.sh
+++ ci_scripts/appveyor_install.sh
@@ -1,3 +1,7 @@
+$CYGWIN\setup-x86_64.exe -qgnNdO -R C:/cygwin64 \
+    -s http://cygwin.mirror.constant.com \
+	-l C:/cygwin64/var/cache/setup \
+	--packages=autotools,make,git,autoconf,automake,bison,yacc
 
 git clone http://git.veripool.org/git/verilator
 cd verilator
