352ffe35bca418be0938eadf006044530fd7cb2f
nic30 <nic30@seznam.cz>
2019-04-20 12:09:58 +0100

    VerilatorWireTC: add some time so it is possible to see res in vcd

--- pycocotb/tests/wire_test.py
+++ pycocotb/tests/wire_test.py
@@ -5,6 +5,7 @@ from pycocotb.verilator.simulator_gen import generatePythonModuleWrapper, \
 from os.path import abspath, dirname, join
 from pycocotb.hdlSimulator import HdlSimulator
 from pycocotb.constants import CLK_PERIOD
+from pycocotb.triggers import Timer
 
 VERILOG_SRCS = dirname(abspath(__file__))
 
@@ -28,7 +29,8 @@ class VerilatorWireTC(unittest.TestCase):
             build_dir,
             accessible_signals)
 
-        sim_module = loadPythonCExtensionFromFile(module_file_name, top_name)
+        sim_module =
+         loadPythonCExtensionFromFile(module_file_name, top_name)
         sim_cls = getattr(sim_module, top_name)
 
         simInstance = sim_cls()
@@ -52,28 +54,30 @@ class VerilatorWireTC(unittest.TestCase):
             io = rtl_sim.io
             sim = HdlSimulator(rtl_sim)
 
-            readed = 0
+            readed = []
             def data_collect(sim):
                 for d_ref in test_data:
                     yield sim.waitReadOnly()
                     d = io.outp.read()
                     d = int(d)
+                    readed.append(d)
                     self.assertEqual(d, d_ref)
-                    readed += 0
+                    yield Timer(CLK_PERIOD)
 
             def data_feed(sim):
                 for d in test_data:
                     yield sim.waitWriteOnly()
                     io.inp.write(d)
+                    yield Timer(CLK_PERIOD)
 
-            # rtl_sim.set_trace_file("cntr.vcd", -1)
-            sim.run(CLK_PERIOD * 10.5,
+            #rtl_sim.set_trace_file("wire%d.vcd" % DW, -1)
+            sim.run(CLK_PERIOD * (len(test_data) + 0.5),
                     extraProcesses=[
                         data_collect,
                         data_feed
                         ]
                     )
-            self.assertEqual(readed, len(test_data))
+            self.assertEqual(len(readed), len(test_data))
 
     def test_wire2(self):
         data = [1, 2, 3, 0, 2, 1, 2, 0, 2]
@@ -90,7 +94,7 @@ class VerilatorWireTC(unittest.TestCase):
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    # suite.addTest(VerilatorCntrTC('test_sim_normal_agents'))
+    # suite.addTest(VerilatorWireTC('test_wire64'))
     suite.addTest(unittest.makeSuite(VerilatorWireTC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
