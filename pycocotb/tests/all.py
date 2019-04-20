b5660930ca5a03eba17c778f4eceb4c45d963b9d
nic30 <nic30@seznam.cz>
2019-04-20 19:43:30 +0100

    create all.py to simply execute all tests

---
+++ pycocotb/tests/all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from unittest import TestLoader, TextTestRunner, TestSuite
+from pycocotb.tests.verilatorCntr_test import VerilatorCntrTC
+from pycocotb.tests.wire_test import VerilatorWireTC
+
+def testSuiteFromTCs(*tcs):
+    loader = TestLoader()
+    loadedTcs = [loader.loadTestsFromTestCase(tc) for tc in tcs]
+    suite = TestSuite(loadedTcs)
+    return suite
+
+
+suite = testSuiteFromTCs(
+    # basic tests
+    VerilatorCntrTC,
+    VerilatorWireTC,
+)
+
+if __name__ == '__main__':
+    runner = TextTestRunner(verbosity=2)
+    runner.run(suite)
