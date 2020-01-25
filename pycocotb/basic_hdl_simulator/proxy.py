6587a7e3ae71b8352c4402fa147ac4fa5511d93c
Nic30 <nic30@seznam.cz>
2020-01-25 13:00:05 +0100

    codestyle

--- pycocotb/basic_hdl_simulator/proxy.py
+++ pycocotb/basic_hdl_simulator/proxy.py
@@ -55,7 +55,11 @@ class BasicRtlSimProxy():
         if t is None:
             val = self._dtype.from_py(val)
         else:
-            val = self._dtype.from_py(val.val, min(val.vld_mask, self._dtype.all_mask()))
+            val = self._dtype.from_py(
+                val.val,
+                min(val.vld_mask,
+                    self._dtype.all_mask())
+            )
         if valueHasChanged(self.val, val):
             self.val = val
             self._propagate_changes()
